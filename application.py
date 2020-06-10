1406e22517071c89d0be784f439c777bb10e3e9b
MYKingma <55245466+MYKingma@users.noreply.github.com>
2020-06-10 14:18:06 +0200

    Style change location page

--- application.py
+++ application.py
@@ -556,7 +556,6 @@ def location(place_id, name):
                 if photo.status_code == 200:
                     location["photo1source"] = data["result"]["photos"][0]["html_attributions"][0]
                     location["photo1"] = photo.url
-                    print(location["photo1source"])
                 if len(data["result"]["photos"]) > 1:
                     photo = requests.get("https://maps.googleapis.com/maps/api/place/photo", params={"key": GOOGLE_API_KEY, "maxwidth": "1000", "photoreference": data["result"]["photos"][1]["photo_reference"]})
                     if photo.status_code == 200:
