510284276db55587fde1f1a0aa0534d2531bfed0
MYKingma <55245466+MYKingma@users.noreply.github.com>
2020-06-16 14:33:04 +0200

    Update run-worker.py

--- run-worker.py
+++ run-worker.py
@@ -1,5 +1,5 @@
 import os
-from urllib.parse import urlparse
+import urllib.parse
 from redis import Redis
 from rq import Queue, Connection
 from rq.worker import HerokuWorker as Worker
@@ -11,7 +11,7 @@ if not redis_url:
     raise RuntimeError('Set up Redis To Go first.')
 
 urlparse.uses_netloc.append('redis')
-url = urlparse.urlparse(redis_url)
+url = urllib.parse.urlparse(redis_url)
 conn = Redis(host=url.hostname, port=url.port, db=0, password=url.password)
 
 if __name__ == '__main__':
