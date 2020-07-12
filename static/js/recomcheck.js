900a98beb74d8d9bf0b04fe05c83fa4a5b5896da
MYKingma <55245466+MYKingma@users.noreply.github.com>
2020-07-12 18:36:01 +0200

    Added recomcheck.js
    For checking review input before publisching recommendation

---
+++ static/js/recomcheck.js
@@ -0,0 +1,26 @@
+/* recomcheck.js for createnew.html and changenew.html
+
+Maurice Kingma
+Minor Programmeren - Web App Studio
+
+Javascript for checking input when publishing
+*/
+
+// wait for page to load
+document.addEventListener('DOMContentLoaded', () => {
+
+    // select submitbutton
+    document.querySelector('input[name="submit"]').onclick = () => {
+
+        // get checkbox and see if it is checked
+        if (document.querySelector('input[name="visible"]').checked) {
+            if (document.getElementById('review').length > 0) {
+                document.querySelector('#review').setCustomValidity('')
+            } else {
+                document.querySelector('#review').setCustomValidity('invalid')
+                document.body.scrollTop = 0; // For Safari
+                document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+            }
+        }
+    };
+});
