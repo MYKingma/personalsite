77432ec5b651c31a4c45d8f8fcaf14968a8524e5
MYKingma <55245466+MYKingma@users.noreply.github.com>
2020-09-28 13:44:24 +0200

    Update application.py
    fix blog_id errors

--- application.py
+++ application.py
@@ -1448,7 +1448,10 @@ def processrequests(request_id):
 def blogpost(blog_id, title):
     if request.method == "GET":
         # get blogpost and render
-        blog = Blog.query.filter_by(id=blog_id).first()
+        if not isinstance(blog_id, int):
+            blog = Blog.query.filter_by(title="Over Stadsgids").first()
+        else:
+            blog = Blog.query.filter_by(id=blog_id).first()
         blogposts = Blog.query.filter(Blog.id != blog_id).filter(Blog.title!="Privacyverklaring").filter(Blog.title!="Over Stadsgids").order_by(Blog.date.desc()).all()
         comments = Comment.query.filter_by(blog_id=blog_id).all()
         return render_template('blogpost.html', blog=blog, blogposts=blogposts, comments=comments)
